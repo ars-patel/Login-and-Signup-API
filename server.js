8c78bee674e9581b84f4b40eb5c9516f14b8b639
ars-patel <arspatel2@gmail.com>
2026-03-04 23:03:56 +0530

    fix: handle CORS preflight for all origins

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // Handle preflight requests for all routes
+app.options(/.*/, cors(corsOptions)); // Handle preflight requests for all routes
 
 app.use(express.json());
 
